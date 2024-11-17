5cee0268f0110f7cbc84f734bdc760f0af8ee92d
Soham Nayak <sohamnayak04@gmail.com>
2024-11-17 09:10:41 +0530

    changes

--- frontend/src/api/tasks.js
+++ frontend/src/api/tasks.js
@@ -1,7 +1,7 @@
 // src/api/tasks.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://task-manager-app-backend-xz3w.onrender.com';
 
 const authAxios = axios.create({
   baseURL: API_URL,
